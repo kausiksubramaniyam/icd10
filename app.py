fcd2fcdedfc18135b4fc99f20d63f167a36188ea
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-12-02 13:11:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for
 import icd10
 
 app=Flask(__name__)
 
+
 @app.route('/',methods=['GET','POST'])
 def homepg():
-	print("HI")
 	return render_template("ihome.html")
 
 
-@app.route('/submit',methods=['GET','POST'])
+@app.route('/submit',methods=['POST','GET'])
 def disp():
 	icdcode=request.form['icdcode']
-	if icd10.exists(icdcode):
-		code=icd10.find(icdcode)
-		if code.billable:
-			a="billable"
-		else:
-			a="not billable"
+	try:
+		if icd10.exists(icdcode):
+			code=icd10.find(icdcode)
+			if code.billable:
+			    a="billable"
+			else:
+				a="not billable"
 
-		return render_template("ihome2.html",icdcode=icdcode,desc=code.description,bill=a,chapter=code.chapter,block=code.block,bdesc=code.block_description)
-	else:
+			return render_template("ihome2.html",icdcode=icdcode,desc=code.description,bill=a,chapter=code.chapter,block=code.block,bdesc=code.block_description)
+
+		else:
+			return render_template("ihome.html",k="Invalid Code, Please Retry!")
+
+	except ValueError:
 		return render_template("ihome.html",k="Invalid Code, Please Retry!")
 
-
-if __name__ == "__main__":
-	app.run(host='0.0.0.0',debug=False)
-	
+if __name__ ==    "__main__":
+	app.run(host="0.0.0.0",debug=False)
