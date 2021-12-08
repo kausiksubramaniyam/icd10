f31c97b910d094ab251444e6eb04343026ac5c2c
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2021-12-08 11:24:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import icd10
+import json
+def code_details(icdcode):
+	keys=["response","code","description","billable","chapter","block","block-description"]
+	details={key: None for key in keys}
+	try:
+		details["code"]=icdcode
+		if icd10.exists(icdcode):
+			details["response"]=1
+			icdcode=icd10.find(icdcode)
+			details["description"]=icdcode.description
+			if icdcode.billable:
+				details["billable"]="Yes"
+			else:
+				details["billable"]="No"
+			details["chapter"]=icdcode.chapter
+			details["block"]=icdcode.block
+			details["block_description"]=icdcode.block_description
+	except ValueError:
+		details["response"]=0
+
+	json_obj=json.dumps(details,indent=4)
+	return json_obj
